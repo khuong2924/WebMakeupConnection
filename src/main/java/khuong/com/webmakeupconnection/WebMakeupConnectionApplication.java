c16089bcd378beab7a817d0ebe24ecd468e2c93e
khuong2924 <truongkhuong666@gmail.com>
2024-11-05 01:35:11 +0700

    first commit

---
+++ src/main/java/khuong/com/webmakeupconnection/WebMakeupConnectionApplication.java
@@ -0,0 +1,13 @@
+package khuong.com.webmakeupconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMakeupConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMakeupConnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/khuong/com/webmakeupconnection/WebMakeupConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package khuong.com.webmakeupconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMakeupConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
